538d188ff698b329d131ee902fdafa8080da990b
Ciprianna <ciprianna17@gmail.com>
2015-07-16 12:46:38 -0500

    Added validation to album and photo models.

--- models/album.rb
+++ models/album.rb
@@ -1,5 +1,6 @@
 # Album Model
 
 class Album < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true
   has_and_belongs_to_many :photos
 end
--- models/photo.rb
+++ models/photo.rb
@@ -1,5 +1,7 @@
 # Photo Model
 
 class Photo < ActiveRecord::Base
+  validates :title, presence: true
+  validates :photographer_id, presence: true
   has_and_belongs_to_many :albums
 end
