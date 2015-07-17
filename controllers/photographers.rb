1e54589943c162b3b96b102e87f6a0391c60ccc4
Ciprianna <ciprianna17@gmail.com>
2015-07-17 11:33:23 -0500

    Can add a photographer

--- controllers/photographers.rb
+++ controllers/photographers.rb
@@ -4,3 +4,24 @@
 get "/photographers" do
   erb :"photographers/photographers"
 end
+
+# ------------------------------------------------------------------------------
+# Add a new photographer
+# ------------------------------------------------------------------------------
+# Step 1 - Display form
+get "/add_photographer" do
+  erb :"photographers/add_photographer"
+end
+
+# Step 2 - Save form info and add photographer to database
+get "/save_new_photographer" do
+  @new_photographer = Photographer.create({"name" => params["photographers"]["name"], "location" => params["photographers"]["location"]})
+
+  if !@new_photographer.errors.empty?
+    @error = true
+    erb :"photographers/add_photographer"
+  else
+    @new_photo
+    erb :"photographers/success"
+  end
+end
