c1bb9dab79a21864ac3cba2e7cf60360d923a070
Ciprianna <ciprianna17@gmail.com>
2015-07-16 21:26:50 -0500

    Can add photos and add a photo to an album

--- controllers/photos.rb
+++ controllers/photos.rb
@@ -17,10 +17,24 @@ end
 get "/save_new_photo" do
   @new_photo = Photo.create({"title" => params["photos"]["title"], "location" => params["photos"]["location"], "photographer_id" => params["photos"]["photographer_id"]})
 
-  if @new_photo.errors.length > 0
+  if !@new_photo.errors.empty?
     @error = true
     erb :"photos/add_photo"
   else
-    erb :"photos/success"
+    @new_photo
+    erb :"photos/add_to_album"
   end
+end
+
+# Step 3 - Add photo to albums
+get "/save_photo_to_albums" do
+  album_list = params["albums"]["album_id"]
+  @new_photo = Photo.find(params["photos"]["id"])
+
+  album_list.each do |id|
+    album = Album.find(id)
+    @new_photo.albums << album
+  end
+
+  erb :"photos/success"
 end
